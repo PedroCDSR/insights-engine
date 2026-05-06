5ea0760fb430d0902309c41f84ac153a6a867295
Pedro Rocha <65837046+PedroCDSR@users.noreply.github.com>
2026-05-05 22:42:23 -0300

    Update index.js

--- index.js
+++ index.js
@@ -48,5 +48,5 @@ app.post('/', async (req, res) => {
 
 const port = process.env.PORT || 8080;
 app.listen(port, '0.0.0.0', () => {
-  console.log(`🚀 Robô online e ouvindo na porta ${port}`);
+  console.log(`🚀 Servidor pronto na porta ${port}`);
 });
